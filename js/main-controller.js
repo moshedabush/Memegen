92989a8be598eaa80adee5c619da63690ee3b34d
Moshe Dabush <moshedabush@h-MacBook-Air-sl-Moshe.local>
2021-08-11 14:03:29 +0300

    added about modal

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,2 +1,18 @@
 'use strict'
 
+function onAboutModal(){
+    var elAboutModal = document.querySelector('.about-modal');
+    var aboutInfo = `<div class="margin-center">
+    <button onclick="onCloseAboutModal()"">X</button>
+    <p>Created by:</p>
+    <p>Moshe Dabush</p>  
+    <p> visit my git hub:</p>
+    <a href=" https://github.com/moshedabush?tab=repositories" target="_blank">To Moshe's github</a>
+    </div>`
+    elAboutModal.innerHTML = aboutInfo;
+    elAboutModal.hidden = false;
+}
+
+function onCloseAboutModal(){
+    let elAboutModal = document.querySelector('.about-modal').hidden = true;
+}
